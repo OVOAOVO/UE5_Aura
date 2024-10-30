9e7ed537086d5ba21acbbc7bfedffed6f1c30073
OVOAOVO <2410651402@qq.com>
2024-10-30 16:24:42 +0800

    Aura Player Controller

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -1,10 +1,11 @@
 // CopyRight OVOAOVO
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
+
+class UInputMappingContext;
 
 /**
  * 
@@ -12,6 +13,15 @@
 UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
-	GENERATED_BODY()
-	
+    GENERATED_BODY()
+
+public:
+    AAuraPlayerController();
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    UPROPERTY(EditAnywhere, Category = "Input")
+    TObjectPtr<UInputMappingContext> AuraContext;
 };
