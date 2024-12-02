3aa53124a61184fd96741b53adcf618cd1293a2c
OVOAOVO <2410651402@qq.com>
2024-12-02 16:10:29 +0800

    Broadcasting Initial Values

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -3,3 +3,11 @@
 
 #include "UI/WidgetController/OverlayWidgetController.h"
 
+
+void UOverlayWidgetController::BroadcastInitialValues()
+{
+	const UAuraAttributeSet* AuraAttrubuteSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	OnHealthChanged.Broadcast(AuraAttrubuteSet->GetHealth());
+	OnMaxHealthChanged.Broadcast(AuraAttrubuteSet->GetMaxHealth());
+}
