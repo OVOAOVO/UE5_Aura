04b75cf49060d872854b1eb004ff675cf723add0
刘浩天 <2410651402@qq.com>
2025-01-02 02:21:08 +0800

    Gameplay Tags Singleton

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// CopyRight OVOAOVO
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"), FString("减少收到的伤害，提高格挡几率"));
+}
