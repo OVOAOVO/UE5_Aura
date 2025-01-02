2910388ecbf71d10c00c7026f8b25af4f6ac36ca
刘浩天 <2410651402@qq.com>
2025-01-03 02:21:02 +0800

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,12 @@
+// CopyRight OVOAOVO
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+}
