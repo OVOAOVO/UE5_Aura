a8f7fd837753f9d8910a192c360f95e5059c1819
刘浩天 <2410651402@qq.com>
2025-01-12 00:40:26 +0800

    Setting Up Click To Move

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -12,6 +12,7 @@ class UInputAction;
 struct FInputActionValue;
 class UAuraInputConfig;
 class UAuraAbilitySystemComponent;
+class USplineComponent;
 
 /**
  * 
@@ -54,4 +55,16 @@ private:
     TObjectPtr<UAuraAbilitySystemComponent> AuraAbilitySystemComponent;
 
     UAuraAbilitySystemComponent* GetASC();
+
+    FVector CachedDestination = FVector::ZeroVector;
+    float FollowTime = 0.f;
+    float ShortPressThreshold = 0.5f;
+    bool bAutoRunning = false;
+    bool bTargeting = false;
+
+    UPROPERTY(EditDefaultsOnly)
+    float AutoRunAcceptanceRadius = 50.f;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<USplineComponent> Spline;
 };
