28c9a855ee9dcd7718eb7a52148e0be4c955723f
刘浩天 <2410651402@qq.com>
2025-01-14 17:11:59 +0800

    Spawn FireBolt from Event

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -12,7 +12,12 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+
+}
+
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
@@ -36,7 +41,4 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 
 		Projectile->FinishSpawning(SpawnTransform);
 	}
-
-
-
 }
